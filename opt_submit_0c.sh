79c88d952fe681fc9de94850ef1171aa2f1c8f83
astro-YYH <yyang440@ucr.edu>
2025-03-06 18:29:45 -0800

    slurm minor

--- opt_submit_0c.sh
+++ opt_submit_0c.sh
@@ -24,7 +24,7 @@ python hyper_optim.py --trials=80 --data_x=./data/pre_N_LHA_z0/train_input.txt -
 python hyper_optim.py --trials=80 --data_x=./data/narrow/matter_power_297_Box25_Part75_27_Box100_Part300_z0/train_input_fidelity_0.txt --data_y=./data/narrow/matter_power_297_Box25_Part75_27_Box100_Part300_z0/train_output_fidelity_0.txt --bound_x=./data/narrow/matter_power_297_Box25_Part75_27_Box100_Part300_z0/input_limits.txt --save_kfold --model_dir=models/pre_N_L2_z0_0c --save_best --lr=0.02 --kfolds=27 --lgk=./data/narrow/matter_power_297_Box25_Part75_27_Box100_Part300_z0/kf.txt --zero_centering &> pre_N_L2_z0_0c.log &
 
 # LH:
-python hyper_optim.py --trials=80 --data_x=./data/pre_N_xL-H_stitch_z0/train_input.txt --data_y=./data/pre_N_xL-H_stitch_z0/train_output.txt --bound_x=./data/pre_N_xL-H_stitch_z0/input_limits.txt --save_kfold --model_dir=models/pre_N_xLH_stitch_z0_0c --save_best --lr=0.01 --kfolds=27 --lgk=./data/pre_N_L-H_stitch_z0/kf.txt --zero_centering &> pre_N_xLH_stitch_z0_0c.log &
+python hyper_optim.py --trials=80 --data_x=./data/pre_N_xL-H_stitch_z0/train_input.txt --data_y=./data/pre_N_xL-H_stitch_z0/train_output.txt --bound_x=./data/pre_N_xL-H_stitch_z0/input_limits.txt --save_kfold --model_dir=models/pre_N_xLH_stitch_z0_0c --save_best --lr=0.02 --kfolds=27 --lgk=./data/pre_N_L-H_stitch_z0/kf.txt --zero_centering &> pre_N_xLH_stitch_z0_0c.log &
 
 sleep 100 
 nvidia-smi
